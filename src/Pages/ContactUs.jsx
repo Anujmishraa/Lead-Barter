95d3dbad8063e67a5e278675b6a3cfb37168f2b2
aunjcoder3716 <anujm3716@gmail.com>
2024-08-12 02:38:37 +0530

    email added

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export const ContactUs = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_xkt3ay8', 'template_5u66tgg', form.current, {
+                publicKey: 'V-N-xtfo8VmKD0nDw',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
     return (
         <>
             <section className="text-gray-600 body-font relative bg-white">
@@ -10,17 +30,17 @@ export const ContactUs = () => {
                         <p className="lg:w-1/3  mx-auto leading-relaxed text-base">We’d love to hear from you! Whether you have a question, feedback, or need support, reaching out to us is easy.</p>
                     </div>
                     <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                        <div className="flex flex-wrap -m-2">
+                        <form ref={form} onSubmit={sendEmail} className="flex flex-wrap -m-2">
                             <div className="p-2 w-1/2">
                                 <div className="relative">
-                                    <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
-                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                                    <label for="user_name" className="leading-7 text-sm text-gray-600">Name</label>
+                                    <input type="text" id="user_name" name="user_name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
-                                    <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
-                                    <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                                    <label for="user_email" className="leading-7 text-sm text-gray-600">Email</label>
+                                    <input type="email" id="user_email" name="user_email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
@@ -31,11 +51,11 @@ export const ContactUs = () => {
                             </div>
                             <div className="p-2 w-full">
                                 <div className='h-8'>
-                                <button className="button2 flex mx-auto text-white bg-[#213e94] border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+                                    <button type='submit' className="button2 flex mx-auto text-white bg-[#213e94] border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
                                 </div>
                             </div>
-                            
-                        </div>
+
+                        </form>
                     </div>
                 </div>
             </section>
